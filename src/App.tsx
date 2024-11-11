c10cb25dc0d4c9e243030a6ccb23140dc48c1313
uxjp <joaopedro1172.com>
2024-11-11 12:41:09 -0300

    feat: Rm unnecessary elements, add my Readme
    - Removed `vite.svg` and `react.svg` files
    - Removed `logo` and `logo-spin` styles from `App.css`
    - Removed `react-logo` style from `App.css`
    - Improved layout and styling in `App.css`
    - Updated `App.tsx` to reflect the changes and remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="app-container">
+      <h2>Hello, it's JP 🦾</h2>
+      <p className='test'>
+        I like to learn new ways to do things and to automate stuff. Trying to
+        do the same thing in a lot of different ways and organize created knowledge... yeah I learn just for the sake of it.  
+        If you have been doing something cool about web dev or DX, contact me.
       </p>
-    </>
-  )
+      <div className="social_links">
+        <a href="https://x.com/uxxxjp" target="_blank">X</a>
+        <a href="https://www.linkedin.com/in/uxjp" target="_blank">LinkedIn</a>
+        <a href="https://github.com/uxjp" target="_blank">GitHub</a>
+      </div>
+      
+      <h4>Now I'm focusing on:</h4>
+      <div className="skills">
+        <img alt="Spring" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" />
+        <img alt="Go" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" />
+        <img alt="React" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+        <img alt="Bash" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" />
+        <img alt="Linux" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" />
+      </div>
+
+      <h3 className='what_i_wrote'>🛠️ What I wrote</h3>
+      <p>
+        I started developing with C/C++ and OpenGL in computer graphics, then moved to full-stack web development using Java, Spring, MSSQL, and Flutter. Now, I'm working with React and Spring Boot to deploy my own apps.
+      </p>
+      <div className="skills">
+        <img alt="Java" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"/>
+        <img alt="Spring" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" />
+        <img alt="Flutter" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" />
+        <img alt="Node.js" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+        <img alt="TypeScript" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" />
+        <img alt="Go" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" />
+        <img alt="Ruby" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" />
+        <img alt="Docker" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
+        <img alt="Git" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+        <img alt="C" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" />
+        <img alt="OpenGL" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opengl/opengl-original.svg" />
+        <img alt="Elixir" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/elixir/elixir-original.svg" />
+        <img alt="Phoenix" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/phoenix/phoenix-original.svg" />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
