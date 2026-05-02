07470b005da829ab62dd6593fe1c511af6a1bdd6
uxjp <joaopedro1172.com>
2026-05-02 20:13:23 +0100

    wip

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,17 @@
+import { useState} from "react"
 import { Link } from "react-router-dom"
 import './Header.css'
 
 function Header() {
+
+  const [open, setOpen] = useState(false)
+
+  const posts = [
+    {id: 1, title: "my first post"},
+    {id: 2, title: "my second post"},
+    {id: 3, title: "my third post"}
+  ]
+
   return (
     <header className="header__header">
 
@@ -11,8 +21,19 @@ function Header() {
         </h1>
 
         <nav className="header__nav-left">
-          <Link to="/">Posts</Link>
+          <div
+            className="dropdown-container"
+            onClick={() => setOpen(!open)}
+          >
+
+            <span>Posts</span>
+
+
+          </div>
+
+
         </nav>
+
       </div>
 
       <nav className="header__right">
