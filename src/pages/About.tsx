25e2a0eeddf05352978f135775aa7d0feae5e046
uxjp <joaopedro1172.com>
2026-05-17 18:36:37 +0100

    axe emoji

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,7 +18,7 @@ function About() {
         <a href="https://github.com/uxjp" target="_blank">GitHub</a>
       </div>
       
-      <h4>Now I'm focusing on:</h4>
+      <h4>What is making me improve my way of thinking the most now:</h4>
       <div className="skills">
         <img alt="Go" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" />
         <img alt="Elixir" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/elixir/elixir-original.svg" />
@@ -31,11 +31,14 @@ function About() {
       </p>
       <div className="skills">
         <img alt="Java" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"/>
+        <img alt="Go" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" />
+        <img alt="Kafka" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apachekafka/apachekafka-original.svg" />
+        <img alt="Quarkus" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/quarkus/quarkus-original.svg" />
         <img alt="Spring" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" />
+        <img alt="AWS" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" />
         <img alt="Flutter" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" />
         <img alt="Node.js" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
         <img alt="TypeScript" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" />
-        <img alt="Go" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" />
         <img alt="Ruby" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" />
         <img alt="Docker" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
         <img alt="Git" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
--- src/pages/About2.tsx
+++ src/pages/About2.tsx
@@ -1,8 +1,14 @@
+import './About2.css'
+
 function About2() {
   return (
     <div className="app-container">
-      <h2>Post 2 Hello, it's JP 🦾</h2>
+      <h2>Post 2 the Reification</h2>
       <p className='test'>
+        Ideas have to come to life. Features have to be understood. 
+        Technical decisions have to use engineering principles in their design.
+        Ideas have to come to life. Features have to be understood. 
+        Technical decisions have to use engineering principles in their design.
         Ideas have to come to life. Features have to be understood. 
         Technical decisions have to use engineering principles in their design.
       </p>
@@ -29,21 +35,6 @@ function About2() {
       <p>
         I started developing with C/C++ and OpenGL in computer graphics, then moved to full-stack web development using Java, Spring, MSSQL, and Flutter. Now, I'm working with React and Spring Boot to deploy my own apps.
       </p>
-      <div className="skills">
-        <img alt="Java" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"/>
-        <img alt="Spring" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg" />
-        <img alt="Flutter" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" />
-        <img alt="Node.js" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-        <img alt="TypeScript" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" />
-        <img alt="Go" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg" />
-        <img alt="Ruby" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" />
-        <img alt="Docker" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
-        <img alt="Git" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-        <img alt="C" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" />
-        <img alt="OpenGL" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opengl/opengl-original.svg" />
-        <img alt="Elixir" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/elixir/elixir-original.svg" />
-        <img alt="Phoenix" width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/phoenix/phoenix-original.svg" />
-      </div>
     </div>
   );
 }
